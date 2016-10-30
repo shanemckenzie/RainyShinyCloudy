2894ae862a33cedeb4afdc6012c60266a9cfc254
Shane McKenzie <shane.mckenzie59@gmail.com>
2016-10-30 15:19:44 -0600

    Setup

---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'RainyShinyCloudy' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for RainyShinyCloudy
+
+  target 'RainyShinyCloudyTests' do
+    inherit! :search_paths
+    # Pods for testing
+    pod ‘Alamofire’,
+        :git => ‘https://github.com/Alamofire/Alamofire.git'
+  end
+
+  target 'RainyShinyCloudyUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ RainyShinyCloudy/WeatherVC.swift
@@ -0,0 +1,31 @@
+//
+//  WeatherVC.swift
+//  RainyShinyCloudy
+//
+//  Created by Shane Mckenzie on 10/26/16.
+//  Copyright © 2016 Shane Mckenzie. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    
+    }
+
+    
+
+}
+
